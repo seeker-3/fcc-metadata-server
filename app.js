ddb1b32b961584a94f42afe18d77093b357c1887
Glitch (fcc-metadata-server) <none>
2018-04-24 18:59:34 +0000

    💠🕍 Checkpoint ./app.js:855406/656 ./views/index2.html:855406/18 ./package.json:855406/19

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const
       log = console.log,
       fs = require("fs"),
       fileUpload = require('express-fileupload'),
+      multer  = require('multer'),
+      upload = multer({ dest: 'uploads/' }),
       express = require('express'),
       app = express()
 ;
 
-app.use(fileUpload());
+//app.use(fileUpload());
 app.listen(process.env.PORT || 3000);
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  log(req.file);
+  res.end();
+})
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index2.html'));
 
-app.post('/upload', (req, res) => {
-  log(req.files.sample);
-  res.end();
-});
+// app.post('/upload', (req, res) => {
+//   log(req.files.sampleFile, req.file);
+//   res.end();
+// });
+
