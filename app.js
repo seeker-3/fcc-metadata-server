af07b51cb27d4f49ebf20123dd5bc22570a4df99
Glitch (fcc-metadata-server) <none>
2018-04-24 16:31:35 +0000

    🚏🍡 Checkpoint ./app.js:855406/399

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const log = console.log;
+const fs = require("fs");
 const express = require('express');
 const app = express();
 
 app.listen(process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.all('/', (req, res) => {
-  log(req.query.file);
-  res.end();
+  if (!req.query.file){
+    res.sendFile(__dirname + '/views/index.html');
+  } else {
+    res.send(fs.statSync(req.query.file).size);
+  }
 });
